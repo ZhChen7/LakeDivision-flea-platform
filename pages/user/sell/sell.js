ec61cf3a9714272b1aaa82dba5d43b617b2475e9
zhouchen <1583741285@qq.com>
2019-11-13 13:56:49 +0800

    add userupdata

--- pages/user/sell/sell.js
+++ pages/user/sell/sell.js
@@ -1,4 +1,4 @@
-// pages/user/user.js
+// pages/user/sell/sell.js
 Page({
 
   /**
@@ -8,18 +8,18 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
   navToPage(event) {
     let route = event.currentTarget.dataset.route
     wx.navigateTo({
       url: route
     })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
 
   /**
